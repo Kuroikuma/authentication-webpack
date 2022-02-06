4c27e696bed4a53b234496b2f293bd4063555cbe
Kuroikuma <odiseoj676@gmail.com>
2022-02-05 22:40:31 -0600

    chore:build

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import render from '../render'
-import { HolaMundo } from '../../frontend/hola-mundo/hola-mundo'
+import { HolaMundo } from '../../frontend/app/containers/hola-mundo'
 
 const main = (req, res, next) => {
+  console.log('estas aqui')
   try {
     const html = renderToString(<HolaMundo />)
     res.send(render(html))
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,13 @@
 import express from 'express'
-import dotenv from 'dotenv'
-import { webpack } from 'webpack'
+import webpack from 'webpack'
 import main from './routes/main'
 import helmet from 'helmet'
 
-dotenv.config()
+require('dotenv').config()
 const ENV = process.env.NODE_ENV
 const PORT = process.env.PORT || 3000
 
 const app = express()
-
 app.use(express.static(`${__dirname}/public`))
 
 if (ENV === 'development') {
@@ -19,10 +17,7 @@ if (ENV === 'development') {
   const webpackHotMiddleware = require('webpack-hot-middleware')
   const compiler = webpack(webpackConfig)
   const serverConfig = {
-    contentBase: `http://localhost:${PORT}`,
-    port: { PORT },
     publicPath: webpackConfig.output.publicPath,
-    hot: true,
     stats: { colors: true },
   }
   app.use(webpackDevMiddleware(compiler, serverConfig))
