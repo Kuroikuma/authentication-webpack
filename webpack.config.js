2ca0eabff38740f4a215689626c4282a796f6484
Kuroikuma <odiseoj676@gmail.com>
2022-02-05 22:36:56 -0600

    chore: añadiendo dependencias y configuraciones del webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,13 @@ const { channel } = require('diagnostics_channel')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
+const dotenv = require('dotenv')
 const TerserPlugin = require('terser-webpack-plugin')
 const webpack = require('webpack')
+
+dotenv.config()
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 const ruleForJavaScript = {
   test: /\.(js|jsx)$/,
@@ -50,62 +55,58 @@ const ruleFormFile = {
 
 const rules = [ruleForJavaScript, ruleForHtml, ruleFormCss, ruleFormFile]
 
-module.exports = (env, argv) => {
-  const { mode } = argv
-  const isProduction = mode === 'production'
-
-  return {
-    entry: './src/frontend/index.js',
-    devtool: isProduction ? 'hidden-source-map' : 'cheap-source-map',
-    output: {
-      path: isProduction
-        ? path.join(process.cwd(), './src/server/public')
-        : '/',
-      filename: 'assets/app.js',
-      publicPath: '/',
-    },
-    optimization: {
-      splitChunks: {
-        chunks: 'async',
-        cacheGroups: {
-          vendors: {
-            name: 'vendors',
-            chunks: 'all',
-            reuseExistingChunk: true,
-            priority: 1,
-            filename: 'assets/vendor.js',
-            enforce: true,
-            test: /[\\/]node_modules[\\/]/,
-          },
+module.exports = {
+  entry: './src/frontend/index.js',
+  devtool: isProduction ? 'hidden-source-map' : 'cheap-source-map',
+  output: {
+    path: isProduction ? path.join(process.cwd(), './src/server/public') : '/',
+    filename: 'assets/app.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      cacheGroups: {
+        vendors: {
+          name: 'vendors',
+          chunks: 'all',
+          reuseExistingChunk: true,
+          priority: 1,
+          filename: 'assets/vendor.js',
+          enforce: true,
+          test: /[\\/]node_modules[\\/]/,
         },
       },
-      minimize: isProduction ? true : false,
-      minimizer: isProduction ? [new TerserPlugin()] : [],
     },
-    module: {
-      rules,
-    },
-    plugins: [
-      isProduction ? () => {} : new webpack.HotModuleReplacementPlugin(),
-      /*  new webpack.LoaderOptionsPlugin({
+    minimize: isProduction ? true : false,
+    minimizer: isProduction ? [new TerserPlugin()] : [],
+  },
+  module: {
+    rules,
+  },
+  plugins: [
+    isProduction ? () => {} : new webpack.HotModuleReplacementPlugin(),
+    /*  new webpack.LoaderOptionsPlugin({
         options: {
           postcss: [autoprefixer()],
         },
       }),*/
-      new MiniCssExtractPlugin({
-        filename: 'assets/css/app.css',
-      }),
-      isProduction
-        ? new CompressionPlugin({
-            test: /\.js$|\.css$/,
-            filename: '[path][base].gz',
-          })
-        : () => {},
-    ],
-    devServer: {
-      open: true,
-      compress: true,
-      historyApiFallback: true,
-    },
-  }
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/app.css',
+    }),
+    isProduction
+      ? new CompressionPlugin({
+          test: /\.js$|\.css$/,
+          filename: '[path][base].gz',
+        })
+      : () => {},
+  ],
+  devServer: {
+    open: true,
+    compress: true,
+    historyApiFallback: true,
+  },
 }
