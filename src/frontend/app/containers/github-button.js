c6ccc2134cc1f0b7700027f976bd211ca304737a
Kuroikuma <odiseoj676@gmail.com>
2022-02-12 22:05:05 -0600

    feat: terminado con auth de github

--- src/frontend/app/containers/github-button.js
+++ src/frontend/app/containers/github-button.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Context from '../context/theme.context'
 import logoWhite from '../.././../assets/github-logo-white.png'
 import logoDark from '../.././../assets/github-logo-dark.png'
@@ -6,6 +6,26 @@ import { ButtonGitHubView } from '../components/github-button'
 
 export const ButtonGitHub = () => {
   const { theme } = useContext(Context)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const url = window.location.href
+      const hashCode = url.includes('?code=')
+      if (hashCode) {
+        const newUrl = url.split('?code=')
+        const code = newUrl[1]
+        fetch(`/auth-github/${code}`)
+          .then((res) => res.json())
+          .then((response) => {
+            console.log(response)
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      }
+    }
+  }, [])
+
   return (
     <ButtonGitHubView logoWhite={logoWhite} logoDark={logoDark} theme={theme} />
   )
