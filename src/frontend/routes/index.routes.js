990c7bf1aa5aa59ba6782d6813a3f31550f0c019
Kuroikuma <odiseoj676@gmail.com>
2022-02-22 00:36:53 -0600

    fix: cambiando nombre de un archivo

--- src/frontend/routes/index.routes.js
+++ src/frontend/routes/index.routes.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import UserContext from '../app/context/user.context'
-import { LoggetOut } from './loggedOut.routes'
+import { LoggetOut } from './LoggedOut.routes'
 import { LoggedIn } from './LoggedIn.routes'
 
 export const IndexRoutes = () => {
