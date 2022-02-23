00088619e4bf736c595b14b84c18cf8536112092
Kuroikuma <odiseoj676@gmail.com>
2022-02-22 20:42:00 -0600

    feat: creando pagina de editar usuario

---
+++ src/frontend/app/pages/edit-profile/title.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../../css/edit-profile.css'
+
+export const Title = ({ children }) => {
+  return (
+    <div className="edit-profile_container__content__title">{children}</div>
+  )
+}
